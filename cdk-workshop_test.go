b0f71bb581a98e5255c11d255f0582b7ac415c63
gonzalgu <gadol.gonzalez@gmail.com>
2024-08-20 19:28:34 -0400

    added pipeline stack

--- cdk-workshop_test.go
+++ cdk-workshop_test.go
@@ -1,25 +1,115 @@
 package main
 
 import (
+	"cdk-workshop/hitcounter"
 	"testing"
 
 	"github.com/aws/aws-cdk-go/awscdk/v2"
 	"github.com/aws/aws-cdk-go/awscdk/v2/assertions"
+	"github.com/aws/aws-cdk-go/awscdk/v2/awslambda"
 	"github.com/aws/jsii-runtime-go"
+	"github.com/google/go-cmp/cmp"
 )
 
-func TestCdkWorkshopStack(t *testing.T) {
+func TestHitCounterConstruct(t *testing.T) {
+	defer jsii.Close()
+	//GIVEN
+	stack := awscdk.NewStack(nil, nil, nil)
+	//WHEN
+	testFn := awslambda.NewFunction(stack, jsii.String("TestFunction"), &awslambda.FunctionProps{
+		Code:    awslambda.Code_FromAsset(jsii.String("lambda"), nil),
+		Runtime: awslambda.Runtime_NODEJS_16_X(),
+		Handler: jsii.String("hello.handler"),
+	})
+
+	hitcounter.NewHitCounter(stack, "MyTestConstruct", &hitcounter.HitCounterProps{
+		Downstream:   testFn,
+		ReadCapacity: 15,
+	})
+
+	//Then
+	template := assertions.Template_FromStack(stack, nil)
+	template.ResourceCountIs(jsii.String("AWS::DynamoDB::Table"), jsii.Number(1))
+}
+
+func TestLambdaFunction(t *testing.T) {
+	defer jsii.Close()
+
 	// GIVEN
-	app := awscdk.NewApp(nil)
+	stack := awscdk.NewStack(nil, nil, nil)
 
 	// WHEN
-	stack := NewCdkWorkshopStack(app, "MyStack", nil)
+	testFn := awslambda.NewFunction(stack, jsii.String("TestFunction"), &awslambda.FunctionProps{
+		Code:    awslambda.Code_FromAsset(jsii.String("lambda"), nil),
+		Runtime: awslambda.Runtime_NODEJS_16_X(),
+		Handler: jsii.String("hello.handler"),
+	})
+	hitcounter.NewHitCounter(stack, "MyTestConstruct", &hitcounter.HitCounterProps{
+		Downstream:   testFn,
+		ReadCapacity: 15,
+	})
 
 	// THEN
 	template := assertions.Template_FromStack(stack, nil)
-
-	template.HasResourceProperties(jsii.String("AWS::SQS::Queue"), map[string]interface{}{
-		"VisibilityTimeout": 300,
+	envCapture := assertions.NewCapture(nil)
+	template.HasResourceProperties(jsii.String("AWS::Lambda::Function"), &map[string]any{
+		"Environment": envCapture,
+		"Handler":     "hitcounter.handler",
 	})
-	template.ResourceCountIs(jsii.String("AWS::SNS::Topic"), jsii.Number(1))
+	expectedEnv := &map[string]any{
+		"Variables": map[string]any{
+			"DOWNSTREAM_FUNCTION_NAME": map[string]any{
+				"Ref": "TestFunction22AD90FC",
+			},
+			"HITS_TABLE_NAME": map[string]any{
+				"Ref": "MyTestConstructHits24A357F0",
+			},
+		},
+	}
+	if !cmp.Equal(envCapture.AsObject(), expectedEnv) {
+		t.Error(expectedEnv, envCapture.AsObject())
+	}
+}
+
+func TestTableCreatedWithEncryption(t *testing.T) {
+	defer jsii.Close()
+
+	stack := awscdk.NewStack(nil, nil, nil)
+	testFn := awslambda.NewFunction(stack, jsii.String("TestFunction"), &awslambda.FunctionProps{
+		Code:    awslambda.Code_FromAsset(jsii.String("lambda"), nil),
+		Runtime: awslambda.Runtime_NODEJS_16_X(),
+		Handler: jsii.String("hello.handler"),
+	})
+	hitcounter.NewHitCounter(stack, "MyTestConstruct", &hitcounter.HitCounterProps{
+		Downstream:   testFn,
+		ReadCapacity: 15,
+	})
+
+	template := assertions.Template_FromStack(stack, nil)
+	template.HasResourceProperties(jsii.String("AWS::DynamoDB::Table"), &map[string]any{
+		"SSESpecification": map[string]any{
+			"SSEEnabled": true,
+		},
+	})
+}
+
+func TestCanPassReadCapacity(t *testing.T) {
+	defer jsii.Close()
+	defer func() {
+		if r := recover(); r == nil {
+			t.Error("Did not throw ReadCapacity error")
+		}
+	}()
+
+	stack := awscdk.NewStack(nil, nil, nil)
+
+	testFn := awslambda.NewFunction(stack, jsii.String("TestFunction"), &awslambda.FunctionProps{
+		Code:    awslambda.Code_FromAsset(jsii.String("lambda"), nil),
+		Runtime: awslambda.Runtime_NODEJS_16_X(),
+		Handler: jsii.String("hello.handler"),
+	})
+	hitcounter.NewHitCounter(stack, "MyTestConstruct", &hitcounter.HitCounterProps{
+		Downstream:   testFn,
+		ReadCapacity: 21,
+	})
 }
---
+++ infra/pipeline-stack.go
@@ -0,0 +1,26 @@
+package infra
+
+import (
+	"github.com/aws/aws-cdk-go/awscdk/v2"
+	"github.com/aws/aws-cdk-go/awscdk/v2/pipelines"
+	"github.com/aws/constructs-go/constructs/v10"
+	"github.com/aws/jsii-runtime-go"
+)
+
+type PipelineStackProps struct {
+	awscdk.StackProps
+}
+
+func NewPipelineStack(scope constructs.Construct, id string, props *PipelineStackProps) awscdk.Stack {
+	var sprops awscdk.StackProps
+	if props != nil {
+		sprops = props.StackProps
+	}
+	stack := awscdk.NewStack(scope, &id, &sprops)
+	pipelines.CodePipelineSource_GitHub(
+		jsii.String("gonzalgu/cdk-workshop-go"),
+		jsii.String("main"),
+		nil,
+	)
+	return stack
+}
